bcf610332666cb23dddd9e1218d8b0425eac1a8f
amine feidi <aminefeidi@hotmail.com>
2021-01-14 22:18:41 +0100

    stats

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ContratRepository;
+use App\Repository\FactureRepository;
+use App\Repository\VoitureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +14,51 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(
+        VoitureRepository $voitureRepository,
+        FactureRepository $factureRepository,
+        ContratRepository $contratRepository
+    ): Response
     {
-        if($this->getUser()){
-            return $this->redirectToRoute("voiture_index");
-        }else{
+        if ($this->getUser()) {
+            $isAdmin = $this->getUser()->getRoles() == ['ROLE_ADMIN'];
+            if ($isAdmin) {
+                return $this->redirectToRoute("voiture_index");
+            } else {
+                $idAgence = $this->getUser()->getAgence()->getId();
+                $nbrVoituresDisp = $voitureRepository->createQueryBuilder('v')
+                    ->where('v.disponibilite = 1 AND v.agence = :agence')
+                    ->setParameters([
+                        'agence' => $idAgence
+                    ])
+                    ->select('count(v.id)')
+                    ->getQuery()
+                    ->getSingleScalarResult();
+
+                $nbrFacturesImp = $factureRepository->createQueryBuilder('f')
+                    ->where('f.payee = 0')
+                    ->select('count(f.id)')
+                    ->getQuery()
+                    ->getSingleScalarResult();
+
+                $nbrVoituresNonRendues = $contratRepository->createQueryBuilder('c')
+                    ->leftJoin('c.voiture', 'v')
+                    ->where('c.dateDeRet < :date AND v.disponibilite = 0 AND v.agence = :agence')
+                    ->setParameters([
+                        'date' => date('Y-m-d'),
+                        'agence' => $idAgence
+                    ])
+                    ->select('count(c.id)')
+                    ->getQuery()
+                    ->getSingleScalarResult();
+
+                return $this->render('index/index.html.twig', [
+                    'voituresDisp' => $nbrVoituresDisp,
+                    'voituresNonRendues' => $nbrVoituresNonRendues,
+                    'facturesImp' => $nbrFacturesImp,
+                ]);
+            }
+        } else {
             return $this->redirectToRoute("app_login");
         }
     }
